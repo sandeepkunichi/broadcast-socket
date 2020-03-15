a8c58b1811a7721d6d4e822a013d364049b52927
Sandeep Kunichi <sandeepkunichi@gmail.com>
2020-03-15 12:34:05 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('message', (data) => {
+        wss.clients.forEach((client) => {
+            client.send(data);
+        });
+    });
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
